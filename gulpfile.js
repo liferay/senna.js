cf51ff53f07353ffef9fa78bed434ea00dd6d5a2
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-12-17 18:57:03 -0300

    Removes IE9 from saucelab, history api is not supported

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,51 @@ metal.registerTasks({
 	bundleFileName: 'senna.js',
 	globalName: 'senna',
 	mainBuildJsTasks: ['build:globals'],
-	moduleName: 'senna'
+	moduleName: 'senna',
+	testSaucelabsBrowsers: {
+		sl_chrome: {
+			base: 'SauceLabs',
+			browserName: 'chrome'
+		},
+		sl_safari: {
+			base: 'SauceLabs',
+			browserName: 'safari'
+		},
+		sl_firefox: {
+			base: 'SauceLabs',
+			browserName: 'firefox'
+		},
+		sl_ie_10: {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 7',
+			version: '10'
+		},
+		sl_ie_11: {
+			base: 'SauceLabs',
+			browserName: 'internet explorer',
+			platform: 'Windows 8.1',
+			version: '11'
+		},
+		sl_iphone: {
+			base: 'SauceLabs',
+			browserName: 'iphone',
+			platform: 'OS X 10.10',
+			version: '7.1'
+		},
+		sl_android_4: {
+			base: 'SauceLabs',
+			browserName: 'android',
+			platform: 'Linux',
+			version: '4.4'
+		},
+		sl_android_5: {
+			base: 'SauceLabs',
+			browserName: 'android',
+			platform: 'Linux',
+			version: '5.0'
+		}
+	}
 });
 
 gulp.task('default', ['build:globals', 'build:amd']);
