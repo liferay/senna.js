30a49f891c5143ad84dab239d6cb9858ee9a028a
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-12-17 17:44:18 -0300

    Updates examples

--- examples/gallery/js/dynamic.js
+++ examples/gallery/js/dynamic.js
@@ -7,8 +7,8 @@
 var app = new senna.App();
 
 app.addSurfaces('preview');
-app.addRoutes(new senna.Route(/\w+\.jpg/, ImageLoaderScreen));
-app.addRoutes(new senna.Route('/examples/gallery/dynamic.html', senna.HtmlScreen));
+app.addRoutes(new senna.Route(/\/examples\/gallery\/static(\d+)?\.html/, senna.HtmlScreen));
+app.addRoutes(new senna.Route(/\w+\.jpg/, customScreenRouteHandler));
 
 /* Stores the value of the next image path during navigation
    ========================================================================== */
@@ -23,34 +23,32 @@ app.on('startNavigate', function(event) {
    Extends Senna's default Screen
    ========================================================================== */
 
-function ImageLoaderScreen() {
-  ImageLoaderScreen.base(this, 'constructor');
-}
-
-senna.inherits(ImageLoaderScreen, senna.Screen);
-
-ImageLoaderScreen.prototype.cached = true;
-ImageLoaderScreen.prototype.getSurfaceContent = function(surfaceId) {
-  if (surfaceId === 'preview') {
-    switch(nextImage) {
-      case '/examples/gallery/img/01.jpg':
-        return getContent('08', '01', '02');
-      case '/examples/gallery/img/02.jpg':
-        return getContent('01', '02', '03');
-      case '/examples/gallery/img/03.jpg':
-        return getContent('02', '03', '04');
-      case '/examples/gallery/img/04.jpg':
-        return getContent('03', '04', '05');
-      case '/examples/gallery/img/05.jpg':
-        return getContent('04', '05', '06');
-      case '/examples/gallery/img/06.jpg':
-        return getContent('05', '06', '07');
-      case '/examples/gallery/img/07.jpg':
-        return getContent('06', '07', '08');
-      case '/examples/gallery/img/08.jpg':
-        return getContent('07', '08', '01');
+function customScreenRouteHandler() {
+  var screenInstance = new senna.Screen();
+  screenInstance.cached = true;
+  screenInstance.getSurfaceContent = function(surfaceId) {
+    if (surfaceId === 'preview') {
+      switch(nextImage) {
+        case '/examples/gallery/img/01.jpg':
+          return getContent('08', '01', '02');
+        case '/examples/gallery/img/02.jpg':
+          return getContent('01', '02', '03');
+        case '/examples/gallery/img/03.jpg':
+          return getContent('02', '03', '04');
+        case '/examples/gallery/img/04.jpg':
+          return getContent('03', '04', '05');
+        case '/examples/gallery/img/05.jpg':
+          return getContent('04', '05', '06');
+        case '/examples/gallery/img/06.jpg':
+          return getContent('05', '06', '07');
+        case '/examples/gallery/img/07.jpg':
+          return getContent('06', '07', '08');
+        case '/examples/gallery/img/08.jpg':
+          return getContent('07', '08', '01');
+      }
     }
-  }
+  };
+  return screenInstance;
 };
 
 /* Returns a fragment with the new image and its previous/next links
