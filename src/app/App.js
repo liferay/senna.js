699953bcf78d6462975646e74e67dccef431e48f
Maira Bello <maira.araujo@liferay.com>
2016-04-20 13:45:57 -0300

    Fixes bug with having "/" route with base path - Fixes #127
    Not sure if this is the best solution, but it works well, since the extra call to `utils.getUrlPathWithoutHash` guarantees that the path will be in the expected format.

--- src/app/App.js
+++ src/app/App.js
@@ -417,7 +417,11 @@ class App extends EventEmitter {
 			return null;
 		}
 
-		path = utils.getUrlPathWithoutHash(path).substr(this.basePath.length);
+		path = utils.getUrlPathWithoutHash(path);
+
+		// Makes sure that the path substring will be in the expected format
+		// (that is, will end with a "/").
+		path = utils.getUrlPathWithoutHash(path.substr(this.basePath.length));
 
 		for (var i = 0; i < this.routes.length; i++) {
 			var route = this.routes[i];
--- test/app/App.js
+++ test/app/App.js
@@ -327,7 +327,9 @@ describe('App', function() {
 			}
 		};
 		this.app.setBasePath('/base');
-		this.app.addRoutes(new Route('/path', Screen));
+		this.app.addRoutes([new Route('/', Screen), new Route('/path', Screen)]);
+		assert.ok(this.app.canNavigate('http://localhost/base/'));
+		assert.ok(this.app.canNavigate('http://localhost/base'));
 		assert.ok(this.app.canNavigate('http://localhost/base/path'));
 		assert.ok(!this.app.canNavigate('http://localhost/base/path1'));
 		assert.ok(!this.app.canNavigate('http://localhost/path'));
