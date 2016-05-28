1a1e36c44cb29cdf00fd602eb81e151ba491e087
michael.bradford@liferay.com <michael.bradford@liferay.com>
2016-05-27 18:06:45 -0700

    Wait until pending navigate is complete before removing screen

--- src/app/App.js
+++ src/app/App.js
@@ -499,7 +499,14 @@ class App extends EventEmitter {
 	handleNavigateError_(path, nextScreen, err) {
 		console.log('Navigation error for [' + nextScreen + '] (' + err + ')');
 		if (!utils.isCurrentBrowserPath(path)) {
-			this.removeScreen(path);
+			 if (this.pendingNavigate) {
+			 	this.pendingNavigate.thenAlways(function() {
+					this.removeScreen(path);
+		 		}, this);
+		 	}
+		 	else {
+		 		this.removeScreen(path);
+		 	}
 		}
 	}
 
