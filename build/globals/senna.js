6d96b71a3f5f09937b6799121763a58a90974099
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2016-06-23 16:29:46 -0700

    Build

--- build/globals/senna.js
+++ build/globals/senna.js
@@ -1,7 +1,7 @@
 /**
  * Senna.js - A blazing-fast Single Page Application engine
- * @author Eduardo Lundgren <edu@rdo.io>
- * @version v1.3.0
+ * @author Liferay, Inc.
+ * @version v1.4.0
  * @link http://sennajs.com
  * @license BSD-3-Clause
  */
