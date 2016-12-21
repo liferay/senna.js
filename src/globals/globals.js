730a12588e9579b738f26de159ffc2d89cfb9657
Ivan Santos <pragmaticivan@gmail.com>
2016-12-20 16:45:28 -0800

    Adds check if window and document are defined before assigning to globals (#182)

--- src/globals/globals.js
+++ src/globals/globals.js
@@ -1,6 +1,11 @@
-var globals = {
-	document: document,
-	window: window
-};
+var globals = globals || {};
+
+if (typeof window !== 'undefined') {
+	globals.window = window;
+}
+
+if (typeof document !== 'undefined') {
+	globals.document = document;
+}
 
 export default globals;
