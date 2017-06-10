52b1aa7eb7fa93231e4b7239958d0d0a786f3b58
Eduardo Lundgren <eduardo.lundgren@gmail.com>
2017-06-09 18:17:04 -0700

    Build

--- build/globals/senna-debug.js
+++ build/globals/senna-debug.js
@@ -1,7 +1,7 @@
 /**
  * Senna.js - A blazing-fast Single Page Application engine
  * @author Liferay, Inc.
- * @version v2.3.0
+ * @version v2.3.1
  * @link http://sennajs.com
  * @license BSD-3-Clause
  */
