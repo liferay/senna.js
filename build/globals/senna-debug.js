9a2dd5b8d2904ed54f64debd24442c8f047f2032
Chema Balsas <jose.balsas@liferay.com>
2017-07-31 09:36:06 +0200

    Build

--- build/globals/senna-debug.js
+++ build/globals/senna-debug.js
@@ -1,7 +1,7 @@
 /**
  * Senna.js - A blazing-fast Single Page Application engine
  * @author Liferay, Inc.
- * @version v2.3.4
+ * @version v2.4.0
  * @link http://sennajs.com
  * @license BSD-3-Clause
  */
