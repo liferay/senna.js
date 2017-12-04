dfd7bff8b5c49b65c9a3dc8319dc4c7a1c2dd00c
Diego Nascimento <diego.nascimento@liferay.com>
2017-12-04 17:25:23 -0300

    Adding version on import/export

--- src/senna.js
+++ src/senna.js
@@ -7,6 +7,7 @@ import HtmlScreen from './screen/HtmlScreen';
 import RequestScreen from './screen/RequestScreen';
 import Route from './route/Route';
 import Screen from './screen/Screen';
+import version from './version';
 
 export default App;
-export { dataAttributeHandler, utils, App, HtmlScreen, Route, RequestScreen, Screen };
+export { dataAttributeHandler, utils, App, HtmlScreen, Route, RequestScreen, Screen, version };
