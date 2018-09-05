24f0790ef02ed607fb11a4bbc670c706c17bb067
Diego Nascimento <diego.nascimento@liferay.com>
2018-09-05 17:50:34 -0300

    Removing unecessary usage of getHref function | Fixes #274

--- src/app/App.js
+++ src/app/App.js
@@ -455,7 +455,7 @@ class App extends EventEmitter {
 
 				if (this.scheduledNavigationQueue.length) {
 					const scheduledNavigation = this.scheduledNavigationQueue.shift();
-					this.maybeNavigate_(utils.getHref(scheduledNavigation.href), scheduledNavigation);
+					this.maybeNavigate_(scheduledNavigation.href, scheduledNavigation);
 				}
 			});
 	}
