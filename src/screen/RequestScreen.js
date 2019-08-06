60136b600199a213651c4a6511f65c96b96773c0
Diego Nascimento <diego.nascimento@liferay.com>
2019-08-06 02:37:54 -0300

    refactor(senna): Missing `default`

--- src/screen/RequestScreen.js
+++ src/screen/RequestScreen.js
@@ -269,6 +269,8 @@ class RequestScreen extends Screen {
           reason.requestError = true;
           reason.requestPrematureTermination = true;
           break;
+        default:
+          break;
       }
       throw reason;
     });
