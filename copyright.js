ae161cf8a8b509f551ba0a5d693101838b553d18
Diego Nascimento <diego.nascimento@liferay.com>
2019-08-07 14:16:15 -0300

    chore(senna): Updates `copyright.js` file to use correct license and applies year dynamically

--- copyright.js
+++ copyright.js
@@ -1,5 +1,5 @@
 /**
- * © 2019 Liferay, Inc. <https://liferay.com>
+ * © <%= YEAR %> Liferay, Inc. <https://liferay.com>
  *
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: BSD-3-Clause
  */
